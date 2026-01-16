305112c8a300824b16a8155fd21947205c1ebb2b
Naithar01 <eeung025@gmail.com>
2026-01-17 00:06:48 +0900

    feat : 테스트케이스 결과 추가 시에 입력 된 result가 올바른 값인지 확인 추가

--- internal/testresult/errros.go
+++ internal/testresult/errros.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrNotFoundTestCase = errors.New("테스트케이스를 찾을 수 없습니다.")
+	ErrInvalidResult    = errors.New("올바르지 않은 결과입니다.")
 )
--- internal/testresult/testresult_service.go
+++ internal/testresult/testresult_service.go
@@ -7,6 +7,7 @@ import (
 
 // TestResultService 인터페이스는 테스트 결과를 관리하는 서비스를 정의합니다.
 type TestResultService interface {
+	IsValidResult(result string) bool
 	Create(req *CreateTestResultRequest, projectID uint) error
 	Find(projectID, testCaseID uint) ([]*model.TestResult, error)
 }
@@ -25,10 +26,20 @@ func NewTestResultService(testResultRepo TestResultRepository, testCaseService t
 	}
 }
 
+// IsValidResult 함수는 result 값이 올바른지 확인합니다.
+func (r *testResultService) IsValidResult(result string) bool {
+	return result == "PASS" || result == "FAIL" || result == "ABORT"
+}
+
 // Create 함수는 새로운 테스트 결과를 생성합니다.
 func (s *testResultService) Create(req *CreateTestResultRequest, projectID uint) error {
 	if testCase, err := s.testCaseService.Find(projectID, req.TestCaseID); err != nil || testCase == nil {
 		return ErrNotFoundTestCase
+	}
+
+	// 입력 된 Result가 검증 된 값중 맞는 값인지 검사
+	if !s.IsValidResult(req.Result) {
+		return ErrInvalidResult
 	}
 
 	testResult := req.ToModel()
